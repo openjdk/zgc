748bc74202389ab910b6841173e94bacc79be2c9
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2023-02-02 11:30:51 +0100

    Add different field tests

--- test/hotspot/jtreg/compiler/gcbarriers/TestZGCBarrierElision.java
+++ test/hotspot/jtreg/compiler/gcbarriers/TestZGCBarrierElision.java
@@ -39,9 +39,9 @@ import jdk.internal.misc.Unsafe;
 class Inner {}
 
 class Outer {
-    // This field is declared volatile to prevent C2 from optimizing memory
-    // accesses away.
-    volatile Inner inner;
+    // Declared volatile to prevent C2 from optimizing memory accesses away.
+    volatile Inner field1;
+    volatile Inner field2;
     public Outer() {}
 }
 
@@ -54,7 +54,8 @@ public class TestZGCBarrierElision {
 
     public static void main(String[] args) {
         TestFramework framework = new TestFramework();
-        Scenario zgc = new Scenario(0, "--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED", "-XX:+UseZGC", "-XX:+UnlockExperimentalVMOptions", "-XX:CompileCommand=quiet",
+        Scenario zgc = new Scenario(0, "--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED",
+                                    "-XX:+UseZGC", "-XX:+UnlockExperimentalVMOptions", "-XX:CompileCommand=quiet",
                                     "-XX:CompileCommand=blackhole,compiler.gcbarriers.TestZGCBarrierElision::blackhole");
         framework.addScenarios(zgc).start();
     }
@@ -68,8 +69,8 @@ public class TestZGCBarrierElision {
         // This blackhole is necessary to prevent C2 from optimizing away the entire body.
         blackhole(o1);
         // Two loads are required, the first one is directly optimized away.
-        blackhole(o1.inner);
-        blackhole(o1.inner);
+        blackhole(o1.field1);
+        blackhole(o1.field1);
     }
 
     @Test
@@ -78,39 +79,46 @@ public class TestZGCBarrierElision {
         Outer o1 = new Outer();
         // This blackhole is necessary to prevent C2 from optimizing away the entire body.
         blackhole(o1);
-        o1.inner = i;
+        o1.field1 = i;
     }
 
     @Test
     @IR(counts = { IRNode.ZLOADP_WITH_BARRIER_FLAG, "strong", "1" }, phase = CompilePhase.FINAL_CODE)
     @IR(counts = { IRNode.ZLOADP_WITH_BARRIER_FLAG, "elided", "1" }, phase = CompilePhase.FINAL_CODE)
     private static void testLoadThenLoad(Outer o, Inner i) {
-        blackhole(o.inner);
-        blackhole(o.inner);
+        blackhole(o.field1);
+        blackhole(o.field1);
     }
 
     @Test
     @IR(counts = { IRNode.ZSTOREP_WITH_BARRIER_FLAG, "strong", "1" }, phase = CompilePhase.FINAL_CODE)
     @IR(counts = { IRNode.ZSTOREP_WITH_BARRIER_FLAG, "elided", "1" }, phase = CompilePhase.FINAL_CODE)
     private static void testStoreThenStore(Outer o, Inner i) {
-        o.inner = i;
-        o.inner = i;
+        o.field1 = i;
+        o.field1 = i;
     }
 
     @Test
     @IR(counts = { IRNode.ZSTOREP_WITH_BARRIER_FLAG, "strong", "1" }, phase = CompilePhase.FINAL_CODE)
     @IR(counts = { IRNode.ZLOADP_WITH_BARRIER_FLAG, "elided", "1" },  phase = CompilePhase.FINAL_CODE)
     private static void testStoreThenLoad(Outer o, Inner i) {
-        o.inner = i;
-        blackhole(o.inner);
+        o.field1 = i;
+        blackhole(o.field1);
     }
 
     @Test
     @IR(counts = { IRNode.ZSTOREP_WITH_BARRIER_FLAG, "strong", "1" }, phase = CompilePhase.FINAL_CODE)
     @IR(counts = { IRNode.ZLOADP_WITH_BARRIER_FLAG, "strong", "1" },  phase = CompilePhase.FINAL_CODE)
     private static void testLoadThenStore(Outer o, Inner i) {
-        blackhole(o.inner);
-        o.inner = i;
+        blackhole(o.field1);
+        o.field1 = i;
+    }
+
+    @Test
+    @IR(counts = { IRNode.ZLOADP_WITH_BARRIER_FLAG, "strong", "2" }, phase = CompilePhase.FINAL_CODE)
+    private static void testLoadThenLoadAnotherField(Outer o, Inner i) {
+        blackhole(o.field1);
+        blackhole(o.field2);
     }
 
     @Run(test = {"testAllocateThenLoad",
@@ -118,14 +126,16 @@ public class TestZGCBarrierElision {
                  "testLoadThenLoad",
                  "testStoreThenStore",
                  "testStoreThenLoad",
-                 "testLoadThenStore"})
-    private void runClassInstanceTests() {
+                 "testLoadThenStore",
+                 "testLoadThenLoadAnotherField"})
+    private void runBasicTests() {
         testAllocateThenLoad(outer, inner);
         testAllocateThenStore(outer, inner);
         testLoadThenLoad(outer, inner);
         testStoreThenStore(outer, inner);
         testStoreThenLoad(outer, inner);
         testLoadThenStore(outer, inner);
+        testLoadThenLoadAnotherField(outer, inner);
     }
 
     @Test
@@ -181,12 +191,22 @@ public class TestZGCBarrierElision {
         U.putReferenceVolatile(o, offset, o1);
     }
 
+    @Test
+    @IR(counts = { IRNode.ZLOADP_WITH_BARRIER_FLAG, "strong", "2" }, phase = CompilePhase.FINAL_CODE)
+    private static void testArrayLoadThenLoadAnotherElement(Outer[] o, Outer o1, Outer o2, Inner i) {
+        long offset = U.arrayBaseOffset(Outer[].class);
+        int scale = U.arrayIndexScale(Outer[].class);
+        blackhole(U.getReferenceVolatile(o, offset));
+        blackhole(U.getReferenceVolatile(o, offset + 10 * scale));
+    }
+
     @Run(test = {"testAllocateArrayThenStoreAtKnownIndex",
                  "testAllocateArrayThenStoreAtUnknownIndex",
                  "testArrayLoadThenLoad",
                  "testArrayStoreThenStore",
                  "testArrayStoreThenLoad",
-                 "testArrayLoadThenStore"})
+                 "testArrayLoadThenStore",
+                 "testArrayLoadThenLoadAnotherElement"})
     private void runArrayTests() {
         testAllocateArrayThenStoreAtKnownIndex(outer, inner);
         testAllocateArrayThenStoreAtUnknownIndex(outer, inner, 10);
@@ -194,5 +214,6 @@ public class TestZGCBarrierElision {
         testArrayStoreThenStore(outerArray, outer, outer, inner);
         testArrayStoreThenLoad(outerArray, outer, outer, inner);
         testArrayLoadThenStore(outerArray, outer, outer, inner);
+        testArrayLoadThenLoadAnotherElement(outerArray, outer, outer, inner);
     }
 }
