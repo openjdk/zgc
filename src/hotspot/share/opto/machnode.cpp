27f049501c1d8ca5c3a048c80061738c602e5656
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2023-02-08 09:53:44 +0100

    Add missing include

--- src/hotspot/share/opto/machnode.cpp
+++ src/hotspot/share/opto/machnode.cpp
@@ -24,6 +24,7 @@
 
 #include "precompiled.hpp"
 #include "gc/shared/barrierSet.hpp"
+#include "gc/shared/c2/barrierSetC2.hpp"
 #include "gc/shared/collectedHeap.hpp"
 #include "memory/universe.hpp"
 #include "oops/compressedOops.hpp"
