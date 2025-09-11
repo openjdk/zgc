d1acf64e326f3ba6d4d936115f9ed50250f4bf88
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2025-09-11 06:51:52 +0000

    UPSTREAM: Add os::elapsed_system_cpu_time

--- src/hotspot/share/runtime/os.hpp
+++ src/hotspot/share/runtime/os.hpp
@@ -296,6 +296,7 @@ class os: AllStatic {
   static jlong elapsed_frequency();
 
   static double elapsed_process_cpu_time();
+  static double elapsed_system_cpu_time();
 
   // Return current local time in a string (YYYY-MM-DD HH:MM:SS).
   // It is MT safe, but not async-safe, as reading time zone
@@ -359,6 +360,7 @@ class os: AllStatic {
   class Machine : AllStatic {
   public:
     static int active_processor_count();
+    static double elapsed_system_cpu_time();
 
     [[nodiscard]] static bool available_memory(physical_memory_size_type& value);
     [[nodiscard]] static bool used_memory(physical_memory_size_type& value);
@@ -379,6 +381,7 @@ class os: AllStatic {
   class Container : AllStatic {
   public:
     [[nodiscard]] static bool processor_count(double& value); // Returns the core-equivalent CPU quota
+    [[nodiscard]] static bool elapsed_system_cpu_time(double& value);
 
     [[nodiscard]] static bool available_memory(physical_memory_size_type& value);
     [[nodiscard]] static bool used_memory(physical_memory_size_type& value);
