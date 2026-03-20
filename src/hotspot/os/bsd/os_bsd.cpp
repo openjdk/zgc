jsikstro/jdk

UPSTREAM: Add os::compressed_memory
This reverts commit 9666853793980340db6b30373f03639b44a2163c.

src/hotspot/os/bsd/os_bsd.cpp
@@ -164,6 +164,22 @@ bool os::Bsd::available_memory(size_t& value) {
   return true;
 }
 
+bool os::Bsd::compressed_memory(size_t& value) {
+#ifdef __APPLE__
+  mach_msg_type_number_t count = HOST_VM_INFO64_COUNT;
+  vm_statistics64_data_t vmstat;
+  kern_return_t kerr = host_statistics64(mach_host_self(), HOST_VM_INFO64,
+                                         (host_info64_t)&vmstat, &count);
+  assert(kerr == KERN_SUCCESS,
+         "host_statistics64 failed - check mach_host_self() and count");
+  if (kerr == KERN_SUCCESS) {
+    value = vmstat.compressor_page_count * os::vm_page_size();
+    return true;
+  }
+#endif
+  return false;
+}
+
 // for more info see :
 // https://man.openbsd.org/sysctl.2
 void os::Bsd::print_uptime_info(outputStream* st) {

src/hotspot/share/runtime/os.cpp
@@ -2226,6 +2226,14 @@ bool os::used_memory(size_t& value) {
   return true;
 }
 
+bool os::compressed_memory(size_t &value) {
+#ifdef __APPLE__
+  return os::Bsd::compressed_memory(value);
+#else
+  return false;
+#endif
+}
+
 bool os::commit_memory(char* addr, size_t bytes, bool executable) {
   assert_nonempty_range(addr, bytes);
   bool res = pd_commit_memory(addr, bytes, executable);

openjdk/zgc

src/hotspot/os/bsd/os_bsd.cpp
@@ -174,6 +174,22 @@ bool os::Bsd::available_memory(physical_memory_size_type& value) {
   return true;
 }
 
+bool os::Bsd::compressed_memory(physical_memory_size_type& value) {
+#ifdef __APPLE__
+  mach_msg_type_number_t count = HOST_VM_INFO64_COUNT;
+  vm_statistics64_data_t vmstat;
+  kern_return_t kerr = host_statistics64(mach_host_self(), HOST_VM_INFO64,
+                                         (host_info64_t)&vmstat, &count);
+  assert(kerr == KERN_SUCCESS,
+         "host_statistics64 failed - check mach_host_self() and count");
+  if (kerr == KERN_SUCCESS) {
+    value = vmstat.compressor_page_count * os::vm_page_size();
+    return true;
+  }
+#endif
+  return false;
+}
+
 // for more info see :
 // https://man.openbsd.org/sysctl.2
 void os::Bsd::print_uptime_info(outputStream* st) {

src/hotspot/share/runtime/os.cpp
@@ -2211,9 +2211,18 @@ bool os::Machine::used_memory(physical_memory_size_type& value) {
   return true;
 }
 
+bool os::compressed_memory(physical_memory_size_type &value) {
+#ifdef __APPLE__
+  return os::Bsd::compressed_memory(value);
+#else
+  return false;
+#endif
+}
+
 #ifndef LINUX
 bool os::is_containerized() {
   return false;
+}
 }
 
 bool os::Container::processor_count(double& value) {