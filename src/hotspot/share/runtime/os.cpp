jsikstro/jdk

UPSTREAM: Add os::compressed_memory
This reverts commit 9666853793980340db6b30373f03639b44a2163c.

src/hotspot/share/runtime/os.cpp
@@ -2226,6 +2226,14 @@ bool os::used_memory(size_t& value) {
   return true;
 }
 
+bool os::compressed_memory(size_t &value) {
+#ifdef __APPLE__
+  return os::Bsd::compressed_memory(value);
+#else
+  return false;
+#endif
+}
+
 bool os::commit_memory(char* addr, size_t bytes, bool executable) {
   assert_nonempty_range(addr, bytes);
   bool res = pd_commit_memory(addr, bytes, executable);

openjdk/zgc

src/hotspot/share/runtime/os.cpp
@@ -2212,9 +2212,18 @@ bool os::Machine::used_memory(physical_memory_size_type& value) {
   return true;
 }
 
+bool os::compressed_memory(physical_memory_size_type &value) {
+#ifdef __APPLE__
+  return os::Bsd::compressed_memory(value);
+#else
+  return false;
+#endif
+}
+
 #ifndef LINUX
 bool os::is_containerized() {
   return false;
+}
 }
 
 bool os::Container::processor_count(double& value) {