40d341885e75bef1b415f0ed6b93d372d2ab3652
Joel Sikström <joel.sikstrom@oracle.com>
2026-04-22 13:27:15 +0200

    Change ZAdaptive in tests as well

--- test/hotspot/jtreg/gc/z/TestAutomaticHeapSizingMode.java
+++ test/hotspot/jtreg/gc/z/TestAutomaticHeapSizingMode.java
@@ -44,7 +44,7 @@ public class TestAutomaticHeapSizingMode {
     private static List<String> createTestSpecificArgs(String... heapSizeArgs) {
       List<String> args = new ArrayList<>();
       args.add("-XX:+UseZGC");
-      args.add("-XX:+ZAdaptive");
+      args.add("-XX:+ZAutomaticHeapSizing");
       args.add("-Xlog:gc+init");
       args.addAll(Arrays.asList(heapSizeArgs));
       args.add("--version");
