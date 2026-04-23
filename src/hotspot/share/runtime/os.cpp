e57d965a2770540f28dfbfd5a5e555c21a0b6f39
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2026-04-23 05:07:01 +0000

    Revert "UPSTREAM: Add os::compressed_memory"
    This reverts commit e841969e10dbd953496670597920471c6e858bb6.

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -2212,14 +2212,6 @@ bool os::Machine::used_memory(physical_memory_size_type& value) {
   return true;
 }
 
-bool os::compressed_memory(physical_memory_size_type &value) {
-#ifdef __APPLE__
-  return os::Bsd::compressed_memory(value);
-#else
-  return false;
-#endif
-}
-
 #ifndef LINUX
 bool os::is_containerized() {
   return false;
--- src/hotspot/share/runtime/os.hpp
+++ src/hotspot/share/runtime/os.hpp
@@ -338,7 +338,6 @@ class os: AllStatic {
   [[nodiscard]] static bool available_memory(physical_memory_size_type& value);
   [[nodiscard]] static bool used_memory(physical_memory_size_type& value);
   [[nodiscard]] static bool free_memory(physical_memory_size_type& value);
-  [[nodiscard]] static bool compressed_memory(physical_memory_size_type& value);
 
   [[nodiscard]] static bool total_swap_space(physical_memory_size_type& value);
   [[nodiscard]] static bool free_swap_space(physical_memory_size_type& value);
